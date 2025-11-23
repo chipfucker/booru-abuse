c11159de6c627691c808da10c518ec184d9e66fc
Belle (Laptop) <nightofthebuttlickers@gmail.com>
2025-11-22 19:36:09 -0600

    use rest param instead of expecting array

--- src/booru/rule34/enum.js
+++ src/booru/rule34/enum.js
@@ -1,21 +1,21 @@
 import Enum from "#lib/enum.js";
 
-export let PostRating = Enum([
+export let PostRating = Enum(
     "SAFE",
     "QUESTIONABLE",
     "EXPLICIT"
-]);
-export let PostStatus = Enum([
+);
+export let PostStatus = Enum(
     "ACTIVE",
     "FLAGGED",
     "DELETED"
-]);
+);
 
-export let TagType = Enum([
+export let TagType = Enum(
     "COPYRIGHT",
     "CHARACTER",
     "ARTIST",
     "GENERAL",
     "METADATA",
     "NULL"
-]);
+);
