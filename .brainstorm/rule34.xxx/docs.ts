db826b9a9db73069e698eea80e4532b9f22d2435
Hazel <141371172+chiptumor@users.noreply.github.com>
2026-01-22 11:57:49 -0600

    docs: brainstorm

--- .brainstorm/rule34.xxx/docs.ts
+++ .brainstorm/rule34.xxx/docs.ts
@@ -18,21 +18,22 @@ declare class Client {
      */
     autocomplete(query: string): Promise<Pick<BaseTag, "name" | "count">[]>
     /**
-     * Returns the post at a given ID.
-     * @param id The ID of the post.
+     * Returns the post at a given Id.
+     * @param id The Id of the post.
      */
-    getPost(id: ID): Promise<Post | null>
+    getPost(id: Id): Promise<Post | null>
     /**
      * Returns a class with methods for accessing each part of a post rather than most of the details at once.  
      * This can be preferred over {@linkcode Client.getPost} for cases such as API limits or when you only need to retrieve certain properties of a post.
-     * @param id The ID of the post.
+     * @param id The Id of the post.
      */
-    makePseudoPost(id: ID): PseudoPost
+    makePseudoPost(id: Id): PseudoPost
     /**
      * Returns an array of posts under a search query.
      * @param options The query used to search for posts.
      */
     search(options: string | SearchOptions): Promise<Post[]> // TODO: what is a 'change id'
+    // TODO: figure out how relevant tags are decided or see if the API supports it
     /**
      * Returns an array of tags listed as relevant to a search.
      * @param options The search query used to get relevant tags.
@@ -40,10 +41,10 @@ declare class Client {
     getRelevantTags(options: Omit<SearchOptions, "perPage">): Promise<PostTag[]>
     getTags(options: { limit: number }): Promise<PostTag[]>
     /**
-     * Returns the tag at a given ID.
-     * @param id The ID of the tag.
+     * Returns the tag at a given Id.
+     * @param id The Id of the tag.
      */
-    getTag(id: ID): Promise<PostTag | null>
+    getTag(id: Id): Promise<PostTag | null>
 }
 
 /**
@@ -56,7 +57,7 @@ declare class Post {
      * The URL of each image is structured like so:
      * 
      * ```plain text
-     * https://api-cdn.rule34.xxx/images/0123/0123456789abcdef.jpg
+     * https://api-cdn.rule34.xxx/images/1234/1234567890abcdef.jpg
      *         ├─────┘            ├────┘ ├──┘ ├──────────────┘ ├─┘
      *         Server reg.        Var.   Dir  Hash             Extension
      * ```
@@ -90,17 +91,19 @@ declare class Post {
         hash: string
     }
 
-    /** The unique ID of the post. */
+    /** The unique Id of the post. */
     id: number
-    /** * The parent post's ID. */
+    /** * The parent post's Id. */
     parent?: number
-    /** The IDs of this post's children. */
+    /** The Ids of this post's children. */
     children: number[]
     /** The total upvotes of this post. */
     score: number
 
-    /** The post's tameness rating. */
+    // TODO: better word for 'maturity?'
+    /** The post's 'maturity' rating. */
     rating: PostRating
+    // TODO: better word for 'display status?'
     /** The post's display status. */
     status: PostStatus
 
@@ -139,10 +142,10 @@ declare class Post {
  * A class with methods that return particular parts of post data.
  */
 declare class PseudoPost {
-    /** The unique ID of the post. */
+    /** The unique Id of the post. */
     id: number
     /**
-     * Whether the ID is valid and associated with an accessible post.  
+     * Whether the Id is valid and associated with an accessible post.  
      * This value is `undefined` until a method of this instance is called.
      */
     valid?: boolean
@@ -157,14 +160,30 @@ declare class PseudoPost {
 }
 
 /**
+ * Tags retrieved from a post.
+ */
+declare class PostTags extends Array<PostTag> {
+    category<T extends TagType>(category: T): PostTag<T>[]
+    text(): string
+}
+
+/**
  * A user on Rule34.
  */
 declare class User {
     /** The username of the user. */
     name: string
-    /** The unique ID of the user. */
+    /** The unique Id of the user. */
     id: number
 }
+
+//#endregion
+
+//#region function
+
+//#endregion
+
+//#region variable
 
 //#endregion
 
@@ -175,7 +194,7 @@ declare class User {
  */
 declare interface Authentication {
     /** The `user_id` parameter of the API key. */
-    user_id: ID
+    user_id: Id
     /** The `api_key` parameter of the API key. */
     api_key: string
 }
@@ -215,7 +234,7 @@ interface DeletedSearchOptions extends BaseSearchOptions {
      */
     deleted: true
     /** @deprecated It is uncertain how exactly this affects deleted post searches. */
-    lastID?: ID
+    lastId?: Id
 }
 
 /**
@@ -228,12 +247,12 @@ declare interface PostFile {
          * Modify the URL to use a different CDN subdomain.
          * @param region The region to use.
          */
-        changeServerRegion(region: PostFileServerRegion): void
+        changeServerRegion(region: CDNServer): void
         /**
          * Return the URL with a different CDN subdomain.
          * @param region The region to use.
          */
-        withServerRegion(region: PostFileServerRegion): string
+        withServerRegion(region: CDNServer): string
     }
     /** The dimensions of the media file. */
     size: [ width: number, height: number ]
@@ -247,10 +266,10 @@ declare interface PostComment {
     author: User
     /** The comment's content. */
     content: string
-    /** The comment's unique ID. */
-    id: ID
-    /** The ID of the post this comment is under. */
-    postId: ID
+    /** The comment's unique Id. */
+    id: Id
+    /** The Id of the post this comment is under. */
+    postId: Id
     /**
      * The date this post was created.
      * @deprecated This value is almost guaranteed to be inaccurate. This always, presumably by mistake, returns the date (specific to the timezone of Rule34's servers) that the comment was fetched.
@@ -261,15 +280,15 @@ declare interface PostComment {
 /**
  * The base interface for Rule34 tags.
  */
-declare interface BaseTag {
+declare interface BaseTag<T extends TagType = TagType> {
     /** The name and value of the tag. */
     name: string
-    /** The unique ID of the tag. */
-    id: ID
+    /** The unique Id of the tag. */
+    id: Id
     /** The amount of posts that use this tag. */
     count: number
     /** The type of this tag. */
-    type: PostTagType
+    type: T
     /** @warning It is uncertain what this property labels. */
     ambiguous: boolean
 }
@@ -278,59 +297,102 @@ declare interface BaseTag {
 
 //#region type
 
-declare type ID = `${number}`
+/** A number used as a unique identifier. */
+declare type Id = number | `${number}`
 
+/** Options used when searching for posts. */
 declare type SearchOptions = ActiveSearchOptions | DeletedSearchOptions
 
+/** Tag used when searching for posts. */
 declare type TagParameter = string | Pick<BaseTag, "name">
 
+/** Query used when searching for posts. */
 declare type SearchQuery = string | TagParameter[]
 
 // TODO
+/** Possible file extensions for a post's media. */
 declare type PostFileExtension =
     | "png"
     | "jpg"
 
+/** Full tag, usually retrieved from getting a tag directly. */
 declare type Tag = BaseTag
 
-declare type PostTag = Pick<BaseTag, "name" | "count" | "type">
-
-declare type PostTags = PostTag[] & { text(): string }
+/** Partial tag retrieved from a post. */
+declare type PostTag<T extends TagType = TagType> = Pick<BaseTag<T>, "name" | "count" | "type">
 
 //#endregion
 
 //#region enum
 
-// TODO
-declare enum PostFileServerRegion {
+// TODO: missing properties
+/**
+ * Rule34's regional CDN servers for media files.  
+ * Used when replacing the subdomain of file URLs.
+ */
+declare enum CDNServer { // TODO: better enum name?
+    /** The image CDN dedicated to APIs. */
     API = "api-cdn",
+    /** The video CDN dedicated to APIs. */
     APIMP4 = "api-cdn-mp4"
 }
+/** Alias for the {@link CDNServer `CDNServer` enum}. */
+declare type PostFileServerRegion = CDNServer
 
+// TODO: better word for 'animation states?'
+/** The possible animation states of a post's media file. */
 declare enum PostFileType {
+    /** A still image&mdash;no animation or audio. */
     Static,
+    /** An animated, looped image&mdash;no playback control or audio. */
     Animated,
+    /** An animated video. */
     Video
 }
 
+// TODO: better word for 'maturity?'
+/** The possible 'maturity' ratings of a post. */
 declare enum PostRating {
+    /**
+     * No explicit material.
+     * @deprecated
+     * This is no longer in use on Rule34, and the only posts you may find with this rating are very recent or already deleted.  
+     * Consider using {@linkcode PostRating.Questionable Questionable} to get safer posts instead.
+     */
     Safe,
+    /** Safe or suggestive material. */
     Questionable,
+    /** Explicit material. */
     Explicit
 }
 
+// TODO: better word for 'display status?'
+/** The possible display statuses of a post. */
 declare enum PostStatus {
+    /** Unmarked. */
     Active,
+    /** Reported and yet to be reviewed. */
     Flagged,
+    /** Not visible. */
     Deleted
 }
 
-declare enum PostTagType {
+/** The possible types a tag can have. */
+declare enum TagType {
+    /** A copyright, such as a series, brand, or franchise. */
     Copyright,
+    /** A character, type of character, or a cover for a vague character description. */
     Character,
+    /** An artist alias, or a tag asking for help finding and tagging with the correct artist, such as `artist_request`. */
     Artist,
+    /** A visual, activity, interest, or a tag that otherwise wouldn't suit any other tag type. */
     General,
+    /** Image metadata, such as dimensions, animation, or calls for help, such as `tagme`. */
     Metadata,
+    /**
+     * An errenous type, such as when a tag's type is labelled `null`.  
+     * Typically displayed as if a {@linkcode TagType.General General} tag.
+     */
     Ambiguous
 }
 
--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 Booru Abuse is an NPM package to make interaction with boorus and other imageboards a breeze.
 
-Check out what's planned in the wiki directory!
+Check out what's planned in the [.brainstorm directory](./.brainstorm/)!
