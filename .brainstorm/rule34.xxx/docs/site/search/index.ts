8fae4e6103d3ec8b31c61365d6c45c0b5c7a1d07
Hazel <141371172+chiptumor@users.noreply.github.com>
2026-01-29 18:37:54 +0000

    docs: move query stuff under search/query folder

--- .brainstorm/rule34.xxx/docs/site/search/index.ts
+++ .brainstorm/rule34.xxx/docs/site/search/index.ts
@@ -1,9 +1,5 @@
-export * from "./enum/query-value"
-
-export * from "./interface/query-options"
-export * from "./interface/query-value-options"
 export * from "./interface/search-options"
 
-export * from "./type/parameter"
 export * from "./type/search-options"
-export * from "./type/search-query"
+
+export * from "./query"
