4a88436f340f83d57452253729d8cdced932af1b
Belle (Laptop) <nightofthebuttlickers@gmail.com>
2026-01-31 03:20:44 -0600

    generic save

--- src/booru/rule34.xxx/client/_class/client-user.ts
+++ src/booru/rule34.xxx/client/_class/client-user.ts
@@ -2,14 +2,12 @@ import { BaseUser } from "../../site/_interface/base-user.ts";
 import { getURL } from "../../site/url/get-url/_function/user.ts";
 
 /** A user tied to a client. */
-export class ClientUser implements Omit<BaseUser, "name"> {
+export class ClientUser implements Omit<BaseUser, "name"|"toURL"> {
     id: number;
 
     constructor (options: ConstructorOptions) {
         this.id = options.id;
     }
-
-    toURL = () => getURL(this.id);
 }
 
 interface ConstructorOptions {
