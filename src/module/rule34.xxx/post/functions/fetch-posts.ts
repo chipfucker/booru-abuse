1e2ad50babd3a93cb5e47adab037d49966c755e0
Belle (Laptop) <nightofthebuttlickers@gmail.com>
2026-02-02 17:58:28 -0600

    feat: add fetchPostsJSON()

--- src/module/rule34.xxx/post/functions/fetch-posts.ts
+++ src/module/rule34.xxx/post/functions/fetch-posts.ts
@@ -1,4 +1,9 @@
 import { shrinkRawXMLSearch } from "./shrink-raw-xml-search.ts";
+import { fetchJSON } from "../../../../util/functions/fetch-json.ts";
 import { fetchXML } from "../../../../util/functions/fetch-xml.ts";
+import type { RawPostJSON } from "../interfaces/raw-post-json.ts";
+import type { RawSearchXML } from "../interfaces/raw-post-xml.ts";
 
-export let fetchPostsXML = (url: string) => fetchXML(url).then(shrinkRawXMLSearch);
+export let fetchPostsJSON = (url: string): Promise<RawPostJSON> => fetchJSON(url);
+
+export let fetchPostsXML = (url: string): Promise<RawSearchXML> => fetchXML(url).then(shrinkRawXMLSearch);
