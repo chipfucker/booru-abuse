cdd92d86f8170312178e1ab61234a3042fe87bce
Hazel <141371172+chiptumor@users.noreply.github.com>
2026-02-02 20:47:07 +0000

    feat: export current items

--- src/module/rule34.xxx/index.ts
+++ src/module/rule34.xxx/index.ts
@@ -1,2 +1,4 @@
+export * from "./client/index.ts";
+export * from "./misc/index.ts";
 export * from "./post/index.ts";
 export * from "./tag/index.ts";
---
+++ src/module/rule34.xxx/misc/index.ts
@@ -0,0 +1,3 @@
+export type { User } from "./classes/user.ts";
+
+export type { BaseUser } from "./interfaces/base-user.ts";
