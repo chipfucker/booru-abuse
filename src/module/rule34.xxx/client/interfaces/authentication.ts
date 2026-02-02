f34cddb2ea586701b6b6ca0948522c42f9e33ff9
Hazel <141371172+chiptumor@users.noreply.github.com>
2026-02-02 18:57:07 +0000

    feat: create Authentication interface

---
+++ src/module/rule34.xxx/client/interfaces/authentication.ts
@@ -0,0 +1,7 @@
+/** The object defining authentication keys for Rule34 API requests. */
+export interface Authentication {
+    /** The `user_id` parameter of the API key. */
+    user_id: number;
+    /** The `api_key` parameter of the API key. */
+    api_key: string;
+}
