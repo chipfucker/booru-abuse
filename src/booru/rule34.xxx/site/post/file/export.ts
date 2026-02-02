f5d994321f841fae1498fac0389b24ae777b053a
Hazel <141371172+chiptumor@users.noreply.github.com>
2026-02-02 17:25:17 +0000

    generic save

--- src/booru/rule34.xxx/site/post/file/export.ts
+++ src/booru/rule34.xxx/site/post/file/export.ts
@@ -1,3 +1,3 @@
 export { PostFileType } from "./_enum/post-file-type.ts";
 
-export type { PostFile } from "./_class/post-file.ts";
+export type { PostFiles, PostFile } from "./_class/post-file.ts";
