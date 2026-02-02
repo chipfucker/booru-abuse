7268ce1726b4f538523af2e1e0cc062818c13931
Belle (Laptop) <nightofthebuttlickers@gmail.com>
2026-02-02 15:53:43 -0600

    feat: create util/createURL()

---
+++ src/util/functions/create-url.ts
@@ -0,0 +1,21 @@
+export function createURL({ base, path = [], params = {} }: URLObject): string {
+    let url = new URL(path.join("/"), base).toString();
+    if (Object.keys(params).length) {
+        type Parameters = NonNullable<URLObject["params"]>;
+        type ParameterEntries = [keyof Parameters, Parameters[keyof Parameters]];
+
+        const object: Parameters = {};
+        Object.entries(params)
+            .filter(([_, value]: ParameterEntries) => value != null)
+            .forEach(([key, value]: ParameterEntries) => object[key] = value);
+
+        url += "?" + new URLSearchParams(object).toString();
+    }
+    return url;
+}
+
+interface URLObject {
+    base: string;
+    path?: [],
+    params?: { [key: string]: any }
+}
