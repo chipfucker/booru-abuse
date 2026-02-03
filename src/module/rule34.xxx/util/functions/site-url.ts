ee388dec6792819089a7215e9abd12d7db60e549
Belle (Laptop) <nightofthebuttlickers@gmail.com>
2026-02-02 21:02:35 -0600

    feat: add search and tagWiki to site-url.ts

--- src/module/rule34.xxx/util/functions/site-url.ts
+++ src/module/rule34.xxx/util/functions/site-url.ts
@@ -1,3 +1,8 @@
 import type { User } from "../../misc/classes/user.ts";
+import type { BaseTag } from "../../tag/interfaces/base-tag.ts";
+
+export let search = (query: string): string => `https://rule34.xxx/index.php?page=post&s=list&tags=${encodeURIComponent(query)}`;
+
+export let tagWiki = (id: BaseTag["id"]): string => `https://rule34.xxx/index.php?page=wiki&s=view&id=${id}`;
 
 export let user = (name: User["name"]): string => `https://rule34.xxx/index.php?page=account&s=profile&uname=${name}`;
