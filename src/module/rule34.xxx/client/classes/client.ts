56529ec711e63bb2895aee691dd0277284ffcc73
Belle (Laptop) <nightofthebuttlickers@gmail.com>
2026-02-02 21:39:10 -0600

    feat: add autocomplete method

--- src/module/rule34.xxx/client/classes/client.ts
+++ src/module/rule34.xxx/client/classes/client.ts
@@ -1,4 +1,5 @@
 import { ClientUser } from "./client-user.ts";
+import { AutocompleteTags } from "../../tag/classes/autocomplete-tags.ts";
 import type { Authentication } from "../interfaces/authentication.ts";
 import type { ClientOptions } from "../interfaces/client-options.ts";
 
@@ -12,5 +13,13 @@ export class Client {
     constructor (options: ClientOptions) {
         this.#auth = options.auth;
         this.self = ClientUser.fromAuth(options.auth);
+    }
+    
+    /**
+     * Returns autocomplete suggestions for a given search.
+     * @param query Search to get autocomplete suggestions for.
+     */
+    async autocomplete(query: string): Promise<AutocompleteTags> {
+        return await AutocompleteTags.fromQuery(query);
     }
 }
