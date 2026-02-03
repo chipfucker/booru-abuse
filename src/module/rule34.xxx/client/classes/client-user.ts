6f63811587f4be26b29ef45771b45f50bf51b2f3
Hazel <141371172+chiptumor@users.noreply.github.com>
2026-02-03 20:13:30 +0000

    refactor: use pick instead of omit for client user

--- src/module/rule34.xxx/client/classes/client-user.ts
+++ src/module/rule34.xxx/client/classes/client-user.ts
@@ -1,7 +1,7 @@
 import type { BaseUser } from "../../misc/interfaces/base-user.ts";
 
 /** A user tied to a client. */
-export class ClientUser implements Omit<BaseUser, "name"|"toURL"> {
+export class ClientUser implements Pick<BaseUser, "id"> {
     id: number;
 
     static fromAuth(object: { user_id: number; }) {
