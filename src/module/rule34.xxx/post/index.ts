7dd50271056281a629b170c1dc844e855c363e77
Hazel <141371172+chiptumor@users.noreply.github.com>
2026-02-03 14:36:42 +0000

    feat: export types

--- src/module/rule34.xxx/post/index.ts
+++ src/module/rule34.xxx/post/index.ts
@@ -1,3 +1,7 @@
 export { PostFileType } from "./enums/post-file-type.ts";
 export { PostRating } from "./enums/post-rating.ts";
 export { PostStatus } from "./enums/post-status.ts";
+
+export type { PostAuthor } from "./classes/post-author.ts";
+export type { PostFiles } from "./classes/post-files.ts";
+export type { Post } from "./classes/post.ts";
--- src/module/rule34.xxx/tag/index.ts
+++ src/module/rule34.xxx/tag/index.ts
@@ -1,1 +1,6 @@
 export { TagType } from "./enums/tag-type.ts";
+
+export type { AutocompleteTag } from "./classes/autocomplete-tag.ts";
+export type { AutocompleteTags } from "./classes/autocomplete-tags.ts";
+
+export type { BaseTag } from "./interfaces/base-tag.ts";
