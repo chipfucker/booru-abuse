06dd62bff6d81b27ace96f713aaaecb41abf44a8
Belle (Laptop) <nightofthebuttlickers@gmail.com>
2026-02-02 18:00:03 -0600

    feat: add typing to constructor and fromObject

--- src/module/rule34.xxx/post/classes/post.ts
+++ src/module/rule34.xxx/post/classes/post.ts
@@ -1,6 +1,8 @@
 import { fetchPostsJSON, fetchPostsXML } from "../functions/fetch-posts.ts";
 import * as api from "../../util/functions/api-url.ts";
 import type { Authentication } from "../../client/interfaces/authentication.ts";
+import type { RawPostJSON } from "../interfaces/raw-post-json.ts";
+import type { RawPostXML } from "../interfaces/raw-post-xml.ts";
 
 export class Post {
     #auth: Authentication;
@@ -13,17 +15,17 @@ export class Post {
             fetchPostsJSON(urls.json), fetchPostsXML(urls.xml)
         ]).then(promises => ({
             json: promises[0],
-            xml: promises[1].posts[0]
+            xml: promises[1].posts[0]!
         }));
 
         return Post.fromObject(response, auth);
     }
 
-    static fromObject(objects: { json: any; xml: any; }, auth: Authentication): Post {
+    static fromObject(objects: { json: RawPostJSON; xml: RawPostXML; }, auth: Authentication): Post {
         return new Post(objects, auth);
     }
 
-    constructor ({ json, xml }: { json: any; xml: any; }, auth: Authentication) {
+    constructor ({ json, xml }: { json: RawPostJSON; xml: RawPostXML; }, auth: Authentication) {
         this.#auth = auth;
     }
 }
