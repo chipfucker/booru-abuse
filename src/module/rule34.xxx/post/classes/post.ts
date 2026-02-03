5674de6fd54bb6269222f496411246e30bcaa4eb
Belle (Laptop) <nightofthebuttlickers@gmail.com>
2026-02-02 19:43:10 -0600

    feat: add properties to post

--- src/module/rule34.xxx/post/classes/post.ts
+++ src/module/rule34.xxx/post/classes/post.ts
@@ -1,13 +1,31 @@
+import { PostFiles } from "./post-files.ts";
+import { PostRating } from "../enums/post-rating.ts";
 import { fetchPostsJSON, fetchPostsXML } from "../functions/fetch-posts.ts";
 import * as api from "../../util/functions/api-url.ts";
-import type { Authentication } from "../../client/interfaces/authentication.ts";
 import type { RawPostJSON } from "../interfaces/raw-post-json.ts";
 import type { RawPostXML } from "../interfaces/raw-post-xml.ts";
+import type { Authentication } from "../../client/interfaces/authentication.ts";
 
 export class Post {
     #auth: Authentication;
     private hasChildren: boolean;
     private commentCount: number;
+
+    /** The media files of the post. */
+    file: PostFiles;
+
+    /** The unique Id of the post. */
+    id: number;
+    /** The parent post's Id. */
+    parent?: number;
+    /**
+     * The source attributed to this post.  
+     * Typically a URL.
+     */
+    source: string;
+    // TODO: better word for 'maturity?'
+    /** The post's 'maturity' rating. */
+    rating: PostRating;
 
     static async fromURL(url: { id: number; }, auth: Authentication): Promise<Post> {
         const urls = api.post(url, auth);
