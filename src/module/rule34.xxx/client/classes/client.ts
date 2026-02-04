29abc872c2d92eed5cd900e2e5a80c56f18531f6
Belle (Laptop) <nightofthebuttlickers@gmail.com>
2026-02-03 22:44:56 -0600

    feat: replace client constructor with static new()

--- src/module/rule34.xxx/client/classes/client.ts
+++ src/module/rule34.xxx/client/classes/client.ts
@@ -1,20 +1,45 @@
 import { ClientUser } from "./client-user.ts";
+import { missingAuthentication } from "../variables/missing-authentication.ts";
 import { Post } from "../../post/classes/post.ts";
 import { Posts } from "../../post/classes/posts.ts";
 import { AutocompleteTags } from "../../tag/classes/autocomplete-tags.ts";
+import * as api from "../../util/functions/api-url.ts";
 import type { Authentication } from "../interfaces/authentication.ts";
 import type { ClientOptions } from "../interfaces/client-options.ts";
 
 /** A client to retrieve date from rule34.xxx. */
 export class Client {
     #auth: Authentication;
+    private authorized: boolean = false;
     
     /** The user tied to the client. */
     self: ClientUser;
 
-    constructor (options: ClientOptions) {
+    /** Returns a client, or throws an error if the credentials are invalid. */
+    static async create(options: ClientOptions): ReturnType<Client["test"]> {
+        return await new Client(options).test();
+    }
+
+    private constructor (options: ClientOptions) {
         this.#auth = options.auth;
         this.self = ClientUser.fromAuth(options.auth);
+    }
+
+    /** Returns the client, or throws an error if the credentials are invalid. */
+    async test(): Promise<this|never> {
+        if (!this.authorized) {
+            const response = await fetch(api.search({
+                tags: "",
+                limit: 0,
+                pid: 0
+            }, this.#auth).json).then(r => r.text());
+
+            if (response === missingAuthentication)
+                throw new Error("Invalid authentication! Make sure you've provided the necessary credentials correctly.");
+            this.authorized = true;
+        }
+            
+        return this;
     }
     
     /**
