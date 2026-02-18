f5e997db18c68a682a664e31e983d4309dde7e46
Hazel <141371172+chiptumor@users.noreply.github.com>
2026-02-18 14:35:05 -0600

    feat: create api-url funcs

---
+++ src/module/rule34/util/functions/api-url.ts
@@ -0,0 +1,42 @@
+import type { Authentication } from "../../client/interfaces/authentication.ts";
+import { createURL } from "../../../../util/functions/create-url.ts";
+import * as apiParameters from "../types/api-url.ts";
+
+let getURL = (
+    s: string,
+    params: NonNullable<Parameters<typeof createURL>[0]["params"]>,
+    auth: Authentication
+) => createURL({
+    base: "https://api.rule34.xxx/",
+    params: {
+        page: "dapi",
+        q: "index",
+        s: s,
+        ...params,
+        ...auth
+    }
+});
+
+export function post(
+    auth: Authentication,
+    params: apiParameters.Search
+): string {
+    return getURL("post", params, auth);
+}
+
+export function postBothFormats(
+    auth: Authentication,
+    params: Omit<apiParameters.Search, "json">
+): string {
+    const xml: apiParameters.Search = params;
+    xml.json = 0;
+    delete xml.fields;
+
+    const json: apiParameters.Search = params;
+    json.json = 1;
+
+    return {
+        xml: post(xml, auth),
+        json: post(json, auth)
+    };
+}
