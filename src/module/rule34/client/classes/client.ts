f8b0855fde68fc7a7c2c487a4590b396f94ad434
Hazel <141371172+chiptumor@users.noreply.github.com>
2026-02-18 14:41:39 -0600

    feat: implement api url func

--- src/module/rule34/client/classes/client.ts
+++ src/module/rule34/client/classes/client.ts
@@ -1,6 +1,7 @@
 import { Authentication } from "../interfaces/authentication.ts";
 import { ClientUser } from "./client-user.ts";
 import type { ClientOptions } from "../interfaces/client-options.ts";
+import * as api from "../../util/functions/api-url.ts";
 
 /** Client to retrieve data from Rule 34 at rule34.xxx. */
 export class Client {
@@ -13,5 +14,17 @@ export class Client {
     constructor (options: ClientOptions) {
         this.#auth = options.auth;
         this.self = ClientUser.fromAuth(options.auth);
+    }
+
+    /**
+     * Returns the client if the credentials are valid, otherwise throws an
+     * error.
+     */
+    async test(): Promise<this | never> {
+        if (!this.authorized) {
+            const response = await fetch(api.post(this.#auth, {
+                limit: 0
+            })).then(r => r.text());
+        }
     }
 }
