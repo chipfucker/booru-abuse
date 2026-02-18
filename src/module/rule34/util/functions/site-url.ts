e9a6a79720285c511ec36f0291ca6b612a824558
Hazel <141371172+chiptumor@users.noreply.github.com>
2026-02-18 12:35:52 -0600

    feat: create util/site-url

---
+++ src/module/rule34/util/functions/site-url.ts
@@ -0,0 +1,21 @@
+export function search(query: string): string {
+    return `https://rule34.xxx/?page=post&s=list&tags=${
+        encodeURIComponent(query) || "all"
+    }`;
+}
+
+export function comment(post: number, id: number, query?: string): string {
+    return `https://rule34.xxx/?page=post&s=view&id=${post}${
+        query
+            ? "&" + encodeURIComponent(query)
+            : ""
+    }#c${id}`;
+}
+
+export function tagWiki(id: number): string {
+    return `https://rule34.xxx/?page=wiki&s=view&id=${id}`;
+}
+
+export function user(name: string): string {
+    return `https://rule34.xxx/?page=account&s=profile&uname=${name}`;
+}
