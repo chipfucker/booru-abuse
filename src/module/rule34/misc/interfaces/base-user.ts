ff4c4dd0313e4280c08b80b30f968aa5a955e283
Hazel <141371172+chiptumor@users.noreply.github.com>
2026-02-18 12:37:30 -0600

    feat: create baseuser

---
+++ src/module/rule34/misc/interfaces/base-user.ts
@@ -0,0 +1,12 @@
+import { user as getURL } from "../../util/functions/site-url.ts";
+
+/** The base interface for user types. */
+export abstract class BaseUser {
+    /** The username of the user. */
+    name: string;
+    /** The unique id of the user. */
+    id: number;
+
+    /** Returns a URL to this user's profile on rule34.xxx. */
+    toURL(): string { return getURL(this.name); }
+}
