005c9486642ba66993ac5fc2b4db09d0c81db4e3
Hazel <141371172+chiptumor@users.noreply.github.com>
2026-02-19 10:46:05 -0600

    feat: add to booruabuseerror

--- src/error/classes/booru-abuse-error.ts
+++ src/error/classes/booru-abuse-error.ts
@@ -1,13 +1,19 @@
-
+import { ERROR_CODE } from "../constants/error-code.ts";
 
 export class BooruAbuseError extends Error {
     /** The unique error code. */
     code: string;
+    /** The hint explaining the error. */
+    hint?: string;
 
-    constructor (code, ...value) {
+    constructor (code: keyof typeof ERROR_CODE, ...args: any[]) {
+        const errorCode = ERROR_CODE[code](...args);
         super();
-
+        
         this.name = this.constructor.name;
         this.code = code;
+        
+        this.message = errorCode.message;
+        this.hint = errorCode.hint;
     }
 }
