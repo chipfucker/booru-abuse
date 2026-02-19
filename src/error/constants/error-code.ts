e7c7f6e089ee82652957dee0a791536e24bf1a21
Hazel <141371172+chiptumor@users.noreply.github.com>
2026-02-19 10:21:10 -0600

    feat: add concat funcs

--- src/error/constants/error-code.ts
+++ src/error/constants/error-code.ts
@@ -1,22 +1,29 @@
+let concat = (joiner: string, ...items: any[]): string => items.join(joiner);
+concat.line  = (...items: any[]): string => concat("\n", ...items);
+concat.space = (...items: any[]): string => concat(" ", ...items);
+
 export const CORE_ERROR = {
     _TEST_ERROR: (...args: any[]) => ({
-        message: "This is a test error.",
-        hint: [
+        message: concat.line(
+            "This is a test error.",
+            `Arguments: ${args.length ? args.join(", ") : "none"}`
+        ),
+        hint: concat.space(
             "You should not see this error thrown; this error type only exists",
             "to test the functionality of errors. If you've come across this",
             "error naturally, file an issue on GitHub as soon as possible."
-        ].join(" ")
+        )
     }),
-    _TEMP: (reason: string, args: any[]) => ({
-        message: [
+    _TEMP: (reason: string, args: { [K: string]: any; }) => ({
+        message: concat.line(
             "This is a valid error whose message hasn't been defined yet.",
             `Case: ${reason}`,
-            `Arguments: ${args.join(", ")}`
-        ].join("\n"),
-        hint: [
+            `Arguments: ${JSON.stringify(args, null, 2)}`
+        ),
+        hint: concat.space(
             "This means that this error case has been considered, but the",
             "message wasn't properly implemented before release. If you've",
             "come across this error, please file an issue on GitHub."
-        ].join(" ")
+        )
     })
 } as const;
