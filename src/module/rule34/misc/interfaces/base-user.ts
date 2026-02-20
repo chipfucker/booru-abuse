4c5c84dcb92c20577aa186d9faee7befe6e49a94
Hazel <141371172+chiptumor@users.noreply.github.com>
2026-02-20 11:06:04 -0600

    fix: set base props as abstract

--- src/module/rule34/misc/interfaces/base-user.ts
+++ src/module/rule34/misc/interfaces/base-user.ts
@@ -3,9 +3,9 @@ import { user as getURL } from "../../util/functions/site-url.ts";
 /** The base interface for users. */
 export abstract class BaseUser {
     /** The username of the user. */
-    name: string;
+    abstract name: string;
     /** The unique id of the user. */
-    id: number;
+    abstract id: number;
 
     /** Returns a URL to this user's profile on rule34.xxx. */
     toURL(): string { return getURL(this.name); }
--- src/module/rule34/tag/interfaces/base-tag.ts
+++ src/module/rule34/tag/interfaces/base-tag.ts
@@ -4,16 +4,16 @@ import { tagWiki as getURL } from "../../util/functions/site-url.ts";
 /** The base interface for tags. */
 export abstract class BaseTag<T extends TagType = TagType> {
     /** The name and value of the tag. */
-    name: string;
+    abstract name: string;
     /** The unique id of the tag. */
-    id: number;
+    abstract id: number;
     /** The amount of posts that use this tag. */
-    count: number;
+    abstract count: number;
     /** The category of this tag. */
-    type: T;
+    abstract type: T;
     // TODO
     /** It is uncertain what this property labels. */
-    ambiguous: boolean;
+    abstract ambiguous: boolean;
 
     /** Returns a URL to this tag's wiki page on rule34.xxx. */
     toWikiURL(): string { return getURL(this.id); }
