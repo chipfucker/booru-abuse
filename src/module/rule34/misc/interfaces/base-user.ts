35cf09444c8b054e05570323de27588e1e8c2680
Hazel <141371172+chiptumor@users.noreply.github.com>
2026-02-20 09:44:47 -0600

    fix: remove errenous text
    MISTAKE: accidentally committed adding the base-tag.ts file

--- src/module/rule34/misc/interfaces/base-user.ts
+++ src/module/rule34/misc/interfaces/base-user.ts
@@ -1,5 +1,5 @@
 import { user as getURL } from "../../util/functions/site-url.ts";
-docs:%20edit%20baseuser%20desc
+
 /** The base interface for users. */
 export abstract class BaseUser {
     /** The username of the user. */
