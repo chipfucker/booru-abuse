f48c3e4e8bbc88fd7dd64747ee90ffc77e39bea0
Hazel <141371172+chiptumor@users.noreply.github.com>
2026-02-20 09:55:12 -0600

    feat: export basetag

--- src/module/rule34/tag/interfaces/base-tag.ts
+++ src/module/rule34/tag/interfaces/base-tag.ts
@@ -1,3 +1,20 @@
 import { TagType } from "../enums/tag-type.ts";
+import { tagWiki as getURL } from "../../util/functions/site-url.ts";
 
 /** The base interface for tags. */
+export abstract class BaseTag<T extends TagType = TagType> {
+    /** The name and value of the tag. */
+    name: string;
+    /** The unique id of the tag. */
+    id: number;
+    /** The amount of posts that use this tag. */
+    count: number;
+    /** The category of this tag. */
+    type: T;
+    // TODO
+    /** It is uncertain what this property labels. */
+    ambiguous: boolean;
+
+    /** Returns a URL to this tag's wiki page on rule34.xxx. */
+    toWikiURL(): string { return getURL(this.id); }
+}
