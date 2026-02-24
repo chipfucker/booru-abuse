18b193e8a0e6bfc78ccf9ed2d4f113cfa20d3a1e
Hazel <141371172+chiptumor@users.noreply.github.com>
2026-02-24 10:50:22 -0600

    feat: add postfiletype

---
+++ src/module/rule34/post/enums/post-file-type.ts
@@ -0,0 +1,9 @@
+/** The possible animation states of a post. */
+export enum PostFileType {
+    /** A still image; no animation or audio. */
+    Static,
+    /** An animated, looped image; no playback control or audio. */
+    Animated,
+    /** An animated video. */
+    Video
+}
