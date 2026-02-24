1493bd634cf5f98ab728f081bc6ac6caecce95ae
Hazel <141371172+chiptumor@users.noreply.github.com>
2026-02-24 11:26:03 -0600

    feat: create raw post interfaces

---
+++ src/module/rule34/api/raw/interface/raw-posts-json.ts
@@ -0,0 +1,47 @@
+interface RawPost {
+    file_url: string;
+    width: number;
+    height: number;
+
+    sample: boolean;
+    sample_url: string;
+    sample_width: number;
+    sample_height: number;
+
+    preview_url: string;
+
+    id: number;
+    owner: string;
+    parent_id: number;
+    has_notes: boolean;
+    rating: "safe" | "questionable" | "explicit";
+    tags: string;
+    source: string;
+
+    change: number;
+    hash: string;
+    directory: number;
+    image: string;
+    status: "active" | "flagged" | "deleted";
+
+    score: number;
+    comment_count: number;
+}
+
+interface TagInfo {
+    tag_info: {
+        count: number;
+        type: "copyright" | "character" | "artist" | "tag" | "metadata" | null;
+        tag: string;
+    }[];
+}
+
+export type RawPostJSON<T extends boolean | unknown = unknown> = RawPost & (
+    T extends true
+        ? TagInfo
+        : T extends false
+            ? {}
+            : Partial<TagInfo>
+);
+
+export type RawPostsJSON = RawPostJSON[];
