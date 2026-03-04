890aa8458e1c17f58f8741800e57716cf3f4edaa
Hazel <141371172+chiptumor@users.noreply.github.com>
2026-03-04 11:41:17 -0600

    docs: add update to xmldom usage

--- src/util/xml/functions/parse-xml.ts
+++ src/util/xml/functions/parse-xml.ts
@@ -5,6 +5,11 @@ export function parseXML(document: string) {
     const xml = parser.parseFromString(document).documentElement;
 
     // xmldom has yet to implement the children property
+
+    // UPDATE: 0.9.9 will implement a ParentNode.children getter
+    // update dependency as soon as possible
+    // https://github.com/xmldom/xmldom/pull/960
+    
     // for now just implement it ourselves
     setChildren(xml);
     
